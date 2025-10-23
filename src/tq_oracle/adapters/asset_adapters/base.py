b0b0bb14c00a144e357a59abbc33f2a17e494c5f
matias-gonz <maigonzalez@fi.uba.ar>
2025-10-23 10:00:06 +0200

    refactor: remove unused AssetAdapter protocol from base asset adapter

--- src/tq_oracle/adapters/asset_adapters/base.py
+++ src/tq_oracle/adapters/asset_adapters/base.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Protocol
 from ...config import OracleCLIConfig
 
 
@@ -12,26 +11,6 @@ class AssetData:
 
     asset_address: str
     amount: int  # in native units
-
-
-class AssetAdapter(Protocol):
-    """Protocol defining the interface for asset adapters."""
-
-    @property
-    def adapter_name(self) -> str:
-        """Return the name of this adapter."""
-        ...
-
-    async def fetch_assets(self, subvault_address: str) -> list[AssetData]:
-        """Fetch asset data for the given vault.
-
-        Args:
-            subvault_address: The subvault contract address to query
-
-        Returns:
-            List of asset data from this protocol
-        """
-        ...
 
 
 class BaseAssetAdapter(ABC):
